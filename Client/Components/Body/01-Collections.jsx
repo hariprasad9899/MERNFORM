95a02489791f5a7163b6dc38fe1daa22f588bd23
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2023-04-16 00:20:58 +0530

    implemented getBooks on client

--- Client/Components/Body/01-Collections.jsx
+++ Client/Components/Body/01-Collections.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./01-CollectionsStyle.scss";
 import data from "./sample";
 import Bookcard from "./02-Bookcard";
+import Axios from "axios";
 
 export default function Collections() {
+    let pilot = data;
+    let [rackData, setRackData] = useState(pilot);
+
+    useEffect(() => {
+        Axios.get("http://localhost:3001/getBooks").then((res) => {
+            setRackData((t) => [...rackData, ...res.data]);
+        });
+    }, []);
+
     return (
         <div className="book-collection">
             <h2 className="head-name">Collections</h2>
             <div className="inner-div">
-                {data.map((item) => (
+                {rackData.map((item) => (
                     <Bookcard data={item} key={item.id} />
                 ))}
             </div>
